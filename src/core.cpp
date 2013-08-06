8da218b063733e877e5da02d25150a340d93140e
Merdosh / Asmyldof <noreply@goibon.com>
2013-08-06 15:21:57 +0200

    Fixed core skeleton duplication .h --> .cpp. Added functions already known through class diagrams in core.h/.cpp, light modification class diagram.

--- src/core.cpp
+++ src/core.cpp
@@ -1,85 +1,201 @@
-class UMSFile {
-	private:
-		
-	protected:
-		
-	public:
+#include "core.h"
+
+using namespace std;
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Start of UMSFile procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+UMSFile::UMSFile()
+{
 
 }
 
-class Person : public UMSFile {
-	private:
-	
-	protected:
-	
-	public:
+UMSFile::~UMSFile()
+{
 
 }
 
-class Configuration: public UMSFile {
-	private:
-
-	protected:
-
-	public:
+UMSFile::SetLocation(LocationType_t Location)
+{
 
 }
 
-class Guest: public Person {
-	private:
-	
-	protected:
-	
-	public:
-	
-	
-}
-
-class User: public Person {
-	private:
-	
-	protected:
-	
-	public:
-	
-	
-}
-
-class SystemConfig: virtual public Configuration {
-	private:
-	
-	protected:
-		
-	public:
-	
-	
-}
-
-class DatabaseConfig: virtual public Configuration {
-	private:
-	
-	protected:
-	
-	public:
-	
-}
-
-class Page: public UMSFile {
-	private:
-
-	protected:
-
-	public:
+UMSFile::ChangeStorageLocation(LocationType_t Location)
+{
 
 }
 
-class MenuWindow: virtual public Page {
-	private:
-
-	protected:
-
-	public:
-
+UMSFile::Store()
+{
 
 }
 
+UMSFile::Store(LocationType_t Location)
+{
+
+}
+
+UMSFile::Load()
+{
+
+}
+
+UMSFile::Load(LocationType_t Location)
+{
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * End of UMSFile procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Start of Person procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+Person::Person()
+{
+
+}
+
+Person::Person(long id)
+{
+
+}
+
+Person::~Person()
+{
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * End of Person procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Start of Configuration procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+Configuration::Configuration()
+{
+	// throw(error:not implemented yet)
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * End of Configuration procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Start of Guest procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+Guest::Guest()
+{
+	
+}
+
+Guest::Guest(long id)
+{
+
+}
+
+Guest::~Guest()
+{
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * End of Guest procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Start of User procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+User::User()
+{
+
+}
+
+User::User(const char* fname /*first name*/,
+		     const char* prepos /*Preposition ('tussenvoegsel')*/,
+		     const char* lname /*last name*/,
+		     const char* pass /*password*/)
+{
+
+}
+
+User::~User()
+{
+
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * End of User procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Start of SystemConfig procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+SystemConfig::SystemConfig()
+{
+	// throw(error: Not Implemented Yet);
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * End of SystemConfig procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Start of DatabaseConfig procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+DatabaseConfig::DatabaseConfig()
+{
+	// throw(error: Not IMplemented YEt)
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * End of DatabaseConfig procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Start of Page procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+Page::Page()
+{
+	// throw(error: Not Implemented Yet);
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * End of Page procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Start of MenuWindow procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+MenuWindow::MenuWindow()
+{
+	// throw(error: Not Implemented Yet);
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * End of MenuWindow procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Start of DeprecatedClass procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+BadClass::BadClass()
+{
+	// throw some error: decide later.
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * End of DeprecatedClass procedures
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
--- src/core.h
+++ src/core.h
@@ -1,84 +1,85 @@
 class UMSFile {
 	private:
-		
+		StorageType_t	StorageType;
+		LocationType_t	LocationType;
 	protected:
-		
+		Configuration*	FileConfig;
 	public:
-
+		UMSFile();
+		~UMSFile();
+		SetLocation(LocationType_t Location);
+		ChangeStorageType(StorageType_t StoreType);
+		Store();
+		Store(LocationType_t Location);
+		Load();
+		Load(LocationType_t Location);
 }
 
 class Person : public UMSFile {
 	private:
-	
-	protected:
-	
+		String FirstName;
+		String Preposition;
+		String LastName;
+		long  Id;
+		Page * LastPage;
 	public:
-
+		Person();
+		Person(long id);
+		~Person();
 }
 
-class Configuration: public UMSFile {
-	private:
-
-	protected:
-
+class Configuration {
 	public:
-
+		Configuration()
 }
 
 class Guest: public Person {
-	private:
-	
-	protected:
-	
 	public:
-	
+		Guest();
+		Guest(long id);
+		~Guest();
 	
 }
 
 class User: public Person {
 	private:
-	
-	protected:
-	
+		String EMail;
+		String PhoneHome;
+		String PhoneMobile;
+		LevelMask_t AdminLevel;
 	public:
-	
+		User(long id);
+		User(const char* fname /*first name*/,
+		     const char* prepos /*Preposition ('tussenvoegsel')*/,
+		     const char* lname /*last name*/,
+		     const char* pass /*password*/);
+		~User();
 	
 }
 
 class SystemConfig: virtual public Configuration {
-	private:
-	
-	protected:
-		
 	public:
-	
+		SystemConfig();
 	
 }
 
 class DatabaseConfig: virtual public Configuration {
-	private:
-	
-	protected:
-	
 	public:
-	
+		DatabaseConfig();
 }
 
 class Page: public UMSFile {
-	private:
-
-	protected:
-
 	public:
-
+		Page();
 }
 
 class MenuWindow: virtual public Page {
-	private:
-
-	protected:
-
 	public:
+		MenuWindow();
 
+}
 
+class BadClass {
+	public:
+		BadClass();
 }
